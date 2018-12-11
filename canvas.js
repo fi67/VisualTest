7aa93559820f7b5e64874f77627130ddc597ae0c
fi67 <fi67@users.noreply.github.com>
2018-12-11 10:49:36 +0000

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,1 +1,14 @@
-console.log("hello");
+let myImage = document.getElementById("myImage");
+let myButtonAdd = document.getElementById("myButtonAdd");
+let myButtonChange = document.getElementById("myButtonChange");
+let myCanvas = document.getElementById("myCanvas");
+let ctx = myCanvas.getContext("2d");
+myButtonChange.addEventListener("click", changeImage,true);
+myButtonAdd.addEventListener("click", addFilter,true);
+
+
+myImage.onload = function() {
+    myCanvas.width = myImage.width;
+   myCanvas.height = myImage.height;
+    ctx.drawImage(myImage, 0, 0, myCanvas.width, myCanvas.height);
+}; 
